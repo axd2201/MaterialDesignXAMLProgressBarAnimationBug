77025b0fb53d742ff5b12f58d1b290e86c2f946b
axD <axd2201@gmail.com>
2017-10-19 02:19:43 -0200

    Project

---
+++ MaterialDesignXAMLProgressBug/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialDesignXAMLProgressBug
+{
+    using System.Windows;
+    using System.Windows.Threading;
+
+    using Caliburn.Micro;
+
+    using MaterialDesignXAMLProgressBug.ViewModels;
+
+    public class Bootstrapper : BootstrapperBase
+    {
+        #region Constructors and Destructors
+
+        public Bootstrapper()
+        {
+            //Initializes WPF
+            this.Initialize();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //Open the Login Window
+            this.DisplayRootViewFor<MainViewModel>();
+        }
+
+        protected override void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show(e.Exception.ToString());
+        }
+
+        #endregion
+    }
+}
---
+++ MaterialDesignXAMLProgressBug/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿// ReSharper disable CompareOfFloatsByEqualityOperator
+namespace MaterialDesignXAMLProgressBug.ViewModels
+{
+    using System.Timers;
+
+    using Caliburn.Micro;
+
+    public class MainViewModel : Screen
+    {
+        private readonly Timer timer = new Timer(5000);
+
+        public MainViewModel()
+        {
+            this.timer.Start();
+            this.timer.Elapsed += this.Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //Giving it a little bit of delay to start the timer
+            if (this.timer.Interval == 5000)
+            {
+                this.timer.Interval = 100;
+            }
+
+            if (this.Progress == 99)
+            {
+                this.timer.Stop();
+                return;
+            }
+            this.Progress++;
+            this.NotifyPropChanges();
+        }
+
+        //Not necessary on this case, but i am replicating my original project
+        private void NotifyPropChanges()
+        {
+            this.NotifyOfPropertyChange(nameof (this.IsInderteminate));
+            this.NotifyOfPropertyChange(nameof (this.Progress));
+        }
+
+        public bool IsInderteminate => this.Progress == 0;
+
+        public uint Progress { get; set; }
+    }
+}
